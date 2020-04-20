be8685d51f3d65cac30147518de2e0fda0acf289
86135 <1428866345@qq.com>
2020-04-20 23:06:50 +0800

    04/20

---
+++ cloud-consumer-order80/src/main/java/springcloud/PaymentMain80.java
@@ -0,0 +1,17 @@
+package springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author : fhr
+ * @Date : 2020/4/17
+ */
+@SpringBootApplication
+public class PaymentMain80{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain80.class, args);
+    }
+
+}
