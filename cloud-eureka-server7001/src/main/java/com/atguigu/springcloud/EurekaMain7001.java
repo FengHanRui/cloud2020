d352ec048fa0f049ae59a007ff72d1343a7c7c5d
fhr <1428866345@qq.com>
2020-04-22 17:08:33 +0800

    4/22 公司：rureka

--- cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -1,20 +1,18 @@
 package com.atguigu.springcloud;
 
-
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author : fhr
- * @Date : 2020/4/17
+ * @Date : 2020/4/22
  */
-
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
-
 }
