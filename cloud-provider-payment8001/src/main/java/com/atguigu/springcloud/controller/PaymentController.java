59b5cf9c6d384c2b449ace84f615c93f0ca6fd6c
fhr <1428866345@qq.com>
2020-04-23 17:09:06 +0800

    4/23 公司：rureka集群

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -6,6 +6,7 @@ import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +20,9 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
 
     //只传给前端CommonResult，不需要前端了解其他的组件
     @PostMapping(value = "/payment/create")
@@ -26,7 +30,7 @@ public class PaymentController {
         int result = paymentService.create(payment);
         log.info("*****插入结果：" + result);
         if (result > 0) {
-            return new CommonResult(200, "插入数据成功,", result);
+            return new CommonResult(200, "插入数据成功,"+serverPort, result);
         } else {
             return new CommonResult(444, "插入数据失败", null);
         }
@@ -38,7 +42,7 @@ public class PaymentController {
         Payment result = paymentService.getPaymentById(id);
         if (result != null) {
             log.info("*****查询结果：" + result);
-            return new CommonResult(200, "查询成功", result);
+            return new CommonResult(200, "查询成功"+serverPort, result);
         } else {
             return new CommonResult(500, "查询失败", null);
         }
