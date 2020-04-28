bbb6add44e9a7a083b9eb6ef9676ef917059fd98
fhr <1428866345@qq.com>
2020-04-28 17:19:26 +0800

    4/28 公司：consul

---
+++ cloud-consumerconsul-order80/src/main/java/springcloud/OrderConsulMain80.java
@@ -0,0 +1,20 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+/**
+ * @Author : fhr
+ * @Date : 2020/4/17
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class, args);
+    }
+
+}
---
+++ cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/PaymentMain8006.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+/**
+ * @Author : fhr
+ * @Date : 2020/4/17
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class, args);
+    }
+
+}
